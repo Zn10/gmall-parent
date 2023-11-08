b78512b33803819dffdeac723643201b50007414
NianZhao <3087715239@qq.com>
2023-11-09 00:28:16 +0800

    014:用户对外接口修复

---
+++ module-management-service/module-service-user/src/main/java/com/zn/gmall/user/mapper/UserAddressMapper.java
@@ -0,0 +1,10 @@
+package com.zn.gmall.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zn.gmall.model.user.UserAddress;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserAddressMapper extends BaseMapper<UserAddress> {
+}
+
---
+++ module-management-service/module-service-user/src/main/java/com/zn/gmall/user/service/api/UserAddressService.java
@@ -0,0 +1,15 @@
+package com.zn.gmall.user.service.api;
+
+import com.zn.gmall.model.user.UserAddress;
+
+import java.util.List;
+
+public interface UserAddressService {
+    /**
+     * 根据用户Id 查询用户的收货地址列表！
+     * @param userId
+     * @return
+     */
+    List<UserAddress> findUserAddressListByUserId(String userId);
+
+}
