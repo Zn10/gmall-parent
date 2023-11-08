5385178613d62d5fdbbae2303ea7dc6ff0191e67
NianZhao <3087715239@qq.com>
2023-11-08 23:54:25 +0800

    010:修复项目无法启动

--- module-management-service/module-service-cart/src/main/java/com/zn/gmall/cart/ServiceCartApplication.java
+++ module-management-service/module-service-cart/src/main/java/com/zn/gmall/cart/ServiceCartApplication.java
@@ -2,7 +2,6 @@ package com.zn.gmall.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * 购物车启动
  */
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @ComponentScan(basePackages = "com.zn.gmall")
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.zn.gmall")
