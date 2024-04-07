b10e64b3427b965b62fefdaa2909b2faff3e6242
赵念 <z3087715239@163.com>
2024-04-08 00:29:28 +0800

    other:限制jar包启动占用内存

--- module-management-common/module-common-service-util/src/main/java/com/zn/gmall/common/handler/GlobalExceptionHandler.java
+++ module-management-common/module-common-service-util/src/main/java/com/zn/gmall/common/handler/GlobalExceptionHandler.java
@@ -17,7 +17,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public Result error(Exception e) {
-        e.printStackTrace();
+        log.error("exception message", e);
         return Result.fail();
     }
 
