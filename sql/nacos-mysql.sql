92306acc1dc4018c54a509dd648ee0d84a26177b
赵念 <3087715239@qq.com>
2024-10-25 20:59:27 +0800

    feat:nacos表增加创建库

--- sql/nacos-mysql.sql
+++ sql/nacos-mysql.sql
@@ -2,6 +2,8 @@
 /*   数据库全名 = nacos_config   */
 /*   表名称 = config_info   */
 /******************************************/
+CREATE DATABASE /**IF NOT EXISTS **/ `nacos_config` /**DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin **/;
+USE `nacos_config`;
 CREATE TABLE `config_info` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
   `data_id` varchar(255) NOT NULL COMMENT 'data_id',
