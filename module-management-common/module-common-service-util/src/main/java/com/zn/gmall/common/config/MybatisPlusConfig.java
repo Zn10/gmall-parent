9c20469ac1a4d27d76091902188444f2e90b94e6
赵念 <3087715239@qq.com>
2024-10-26 23:01:47 +0800

    fix:增加分页拦截器，完整sql输出

--- module-management-common/module-common-service-util/src/main/java/com/zn/gmall/common/config/MybatisPlusConfig.java
+++ module-management-common/module-common-service-util/src/main/java/com/zn/gmall/common/config/MybatisPlusConfig.java
@@ -1,6 +1,7 @@
 package com.zn.gmall.common.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,8 +20,8 @@ public class MybatisPlusConfig {
      * 分页插件
      */
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+    public PaginationInnerInterceptor paginationInterceptor() {
+        PaginationInnerInterceptor paginationInterceptor = new PaginationInnerInterceptor();
         // paginationInterceptor.setLimit(你的最大单页限制数量，默认 500 条，小于 0 如 -1 不受限制);
         return paginationInterceptor;
     }
---
+++ module-management-common/module-common-service-util/src/main/java/com/zn/gmall/common/interceptor/ExecteSqlLogInterceptor.java
@@ -0,0 +1,122 @@
+package com.zn.gmall.common.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.type.TypeHandlerRegistry;
+import org.springframework.stereotype.Component;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.ibatis.executor.Executor;
+
+/**
+ * Package: com.zn.gmall.common.interceptor
+ * Description:
+ * Created on 2024-10-26 22:26
+ *
+ * @author zhaonian
+ */
+@Intercepts({
+        @Signature(type = Executor.class, method = "update", args = {
+                MappedStatement.class, Object.class}),
+        @Signature(type = Executor.class, method = "query", args = {
+                MappedStatement.class, Object.class, RowBounds.class,
+                ResultHandler.class})})
+@Slf4j
+@Component
+public class ExecteSqlLogInterceptor implements Interceptor {
+
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
+        Object parameter = null;
+        if (invocation.getArgs().length > 1) {
+            parameter = invocation.getArgs()[1];
+        }
+        BoundSql boundSql = mappedStatement.getBoundSql(parameter);
+        Configuration configuration = mappedStatement.getConfiguration();
+        String sqlId = mappedStatement.getId();
+        int index = sqlId.indexOf("mapper");
+        if (index != -1) {
+            sqlId = sqlId.substring(index + 7);
+        }
+        long start = System.currentTimeMillis();
+
+        Object returnValue = invocation.proceed();
+
+        long end = System.currentTimeMillis();
+
+        long time = end - start;
+        if (time > 1) {
+            String exexcSql = getSql(configuration, sqlId, time, boundSql);
+            log.info("完整exexcSql执行: {}", exexcSql);
+        }
+        return returnValue;
+    }
+
+    private static String getRuntimeExeSql(Configuration configuration, BoundSql boundSql) {
+        Object parameterObject = boundSql.getParameterObject();
+        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+        String sql = boundSql.getSql().replaceAll("[\\s]+", " ");
+        if (!parameterMappings.isEmpty() && parameterObject != null) {
+            TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
+            if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
+                sql = sql.replaceFirst("\\?", getParameterValue(parameterObject));
+            } else {
+                MetaObject metaObject = configuration.newMetaObject(parameterObject);
+                for (ParameterMapping parameterMapping : parameterMappings) {
+                    String propertyName = parameterMapping.getProperty();
+                    if (metaObject.hasGetter(propertyName)) {
+                        Object obj = metaObject.getValue(propertyName);
+                        sql = sql.replaceFirst("\\?", getParameterValue(obj));
+                    } else if (boundSql.hasAdditionalParameter(propertyName)) {
+                        Object obj = boundSql.getAdditionalParameter(propertyName);
+                        sql = sql.replaceFirst("\\?", getParameterValue(obj));
+                    }
+                }
+            }
+        }
+        return sql;
+    }
+
+    private static String getParameterValue(Object obj) {
+        String value = null;
+        if (obj instanceof String) {
+            value = "'" + obj + "'";
+        } else if (obj instanceof Date) {
+            DateFormat formatter = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, Locale.CHINA);
+            value = "'" + formatter.format(new Date()) + "'";
+        } else {
+            if (obj != null) {
+                value = obj.toString();
+            } else {
+                value = "";
+            }
+        }
+        return value;
+    }
+
+    private String getSql(Configuration configuration, String sqlId, long time, BoundSql boundSql) {
+        String sql = getRuntimeExeSql(configuration, boundSql);
+        StringBuilder result = new StringBuilder();
+        result.append(" [" + sqlId + "] ");
+        result.append(" ==> ");
+        result.append(sql);
+        result.append(" ==> ");
+        result.append(time);
+        result.append("ms");
+        return result.toString();
+    }
+}
