076fcc84cc60a250e33458935cce8d17412ab606
赵念 <3087715239@qq.com>
2024-10-28 16:29:59 +0800

    feat:格式化代码

--- module-management-feign/module-feign-api-product/src/main/java/com/zn/gmall/product/client/ProductDegradeFeignClient.java
+++ module-management-feign/module-feign-api-product/src/main/java/com/zn/gmall/product/client/ProductDegradeFeignClient.java
@@ -15,18 +15,14 @@ public class ProductDegradeFeignClient implements ProductFeignClient {
     @Override
     public Result<BaseTrademark> getTrademarkById(Long id) {
         Result<BaseTrademark> result = Result.fail();
-
         result.message("服务降级");
-
         return result;
     }
 
     @Override
     public Result<List<BaseAttrInfo>> getBaseAttrInfoBySkuId(Long skuId) {
         Result<List<BaseAttrInfo>> result = Result.fail();
-
         result.message("服务降级");
-
         return result;
     }
 
@@ -39,69 +35,50 @@ public class ProductDegradeFeignClient implements ProductFeignClient {
 
     @Override
     public Result<SkuInfo> getSkuInfo(Long skuId) {
-
         Result<SkuInfo> result = Result.fail();
-
         result.message("服务降级");
-
         return result;
     }
 
     @Override
     public Result<BaseCategoryView> getCategoryView(Long category3Id) {
-
         Result<BaseCategoryView> result = Result.fail();
-
         result.message("服务降级");
-
         return result;
     }
 
     @Override
     public Result<BigDecimal> getSkuPrice(Long skuId) {
-
         Result<BigDecimal> result = Result.fail();
-
         result.message("服务降级");
-
         return result;
     }
 
     @Override
     public Result<List<SpuSaleAttr>> getSpuSaleAttrListCheckBySku(Long skuId, Long spuId) {
-
         Result<List<SpuSaleAttr>> result = Result.fail();
-
         result.message("服务降级");
-
         return result;
     }
 
     @Override
     public Result<Map<String, Object>> getSkuValueIdsMap(Long spuId) {
-
         Result<Map<String, Object>> result = Result.fail();
-
         result.message("服务降级");
-
         return result;
     }
 
     @Override
     public Result<List<SpuPoster>> getSpuPosterBySpuId(Long spuId) {
         Result<List<SpuPoster>> result = Result.fail();
-
         result.message("服务降级");
-
         return result;
     }
 
     @Override
     public Result<List<BaseAttrInfo>> getAttrList(Long skuId) {
         Result<List<BaseAttrInfo>> result = Result.fail();
-
         result.message("服务降级");
-
         return result;
     }
 }
