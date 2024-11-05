c9757e06b2bab749faf9b90678c0670f9d4c3939
赵念 <3087715239@qq.com>
2024-11-05 16:27:42 +0800

    feat:商品详情服务增加必要参数空指针判断

--- module-management-service/module-service-item/src/main/java/com/zn/gmall/item/controller/api/ItemApiController.java
+++ module-management-service/module-service-item/src/main/java/com/zn/gmall/item/controller/api/ItemApiController.java
@@ -2,6 +2,7 @@ package com.zn.gmall.item.controller.api;
 
 import com.zn.gmall.common.result.Result;
 import com.zn.gmall.item.service.api.ItemService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/item")
+@Slf4j
 public class ItemApiController {
     @Autowired
     private ItemService itemService;
@@ -22,7 +24,11 @@ public class ItemApiController {
      * @param skuId 商品SKUID
      */
     @GetMapping("{skuId}")
-    public Result<Map<String,Object>> getItem(@PathVariable Long skuId) {
+    public Result<Map<String, Object>> getItem(@PathVariable Long skuId) {
+        log.info("获取商品详情信息，skuId:{}", skuId);
+        if (skuId == null) {
+            return Result.<Map<String, Object>>fail().message("skuid为空");
+        }
         Map<String, Object> result = itemService.getBySkuId(skuId);
         return Result.ok(result);
     }
