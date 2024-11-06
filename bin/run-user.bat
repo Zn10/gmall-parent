555e1d6798787e25b9005dd0a3db5bf168cf7b90
赵念 <3087715239@qq.com>
2024-11-06 22:29:47 +0800

    feat:脚本启动减少内存

--- bin/run-user.bat
+++ bin/run-user.bat
@@ -6,7 +6,7 @@ echo.
 cd %~dp0
 cd ../module-management-service/module-service-user/target
 
-set JAVA_OPTS=-Xms128m -Xmx512m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
+set JAVA_OPTS=-Xms128m -Xmx256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m
 
 java -Dfile.encoding=utf-8 %JAVA_OPTS% -jar module-service-user.jar
 
