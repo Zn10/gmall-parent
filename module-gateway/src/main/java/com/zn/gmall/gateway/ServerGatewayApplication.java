fd4b35cd1115d702fa52ba310805f45d5329c451
赵念 <3087715239@qq.com>
2026-06-15 17:00:33 +0800

    feat:网关服务增加启动成功标识

--- module-gateway/src/main/java/com/zn/gmall/gateway/ServerGatewayApplication.java
+++ module-gateway/src/main/java/com/zn/gmall/gateway/ServerGatewayApplication.java
@@ -1,12 +1,15 @@
 package com.zn.gmall.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ServerGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerGatewayApplication.class, args);
+        log.info("网关服务启动成功");
     }
 }
 
